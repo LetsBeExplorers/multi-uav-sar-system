5594b66969c07d476100e4fa58996f9b4d148824
raytracer <ray3618@gmail.com>
2026-03-18 23:47:33 -0600

    coordination node shouldnt publish done, thats confused tf out of me

--- src/swarm_coordination/swarm_coordination/swarm_coordinator.py
+++ src/swarm_coordination/swarm_coordination/swarm_coordinator.py
@@ -217,12 +217,9 @@ class SwarmCoordinator(Node):
 
         # Stop Logging if reaches 100% coverage
         if coverage >= 0.9999:
-            self.set_state("DONE")
-            
             if self.timer is not None:
                 self.destroy_timer(self.timer)
                 self.timer = None
-
             return
 
         with open(self.results_file, "a") as f:
