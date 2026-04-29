0af27216079768bb55fd39739f82681c29ec05e6
raytracer <ray3618@gmail.com>
2026-04-29 16:48:02 -0600

    not needed anymore because fsm doesnt publish targets

--- src/swarm_coordination/swarm_coordination/uav_state_manager.py
+++ src/swarm_coordination/swarm_coordination/uav_state_manager.py
@@ -75,7 +75,6 @@ class UAVStateManager(Node):
 
         # ===== Publishers =====
         self._state_pub = self.create_publisher(UAVState, '/uav/state', 10)
-        self._target_pub = self.create_publisher(String, '/fsm/target_detected', 10)
         self._command_pub = self.create_publisher(FSMEvent, f'/{self.uav_id}/fsm/command', 10)
 
         # ===== Subscribers =====
@@ -258,7 +257,6 @@ class UAVStateManager(Node):
 
         # target lock
         if new_state == 'TARGET_LOCK':
-            self._publish_target_detected()
             self._publish_command('STOP')
 
         # returning
