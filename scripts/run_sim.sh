133e6d2f4444ca0357349fcdb2b56c72cbadbc0c
raytracer <ray3618@gmail.com>
2026-04-30 22:45:15 -0600

    gazebo needs to start unpaused

--- scripts/run_sim.sh
+++ scripts/run_sim.sh
@@ -18,7 +18,7 @@ source /opt/ros/jazzy/setup.bash
 
 echo "Launching Gazebo simulation..."
 WORLD_FILE=${1:-quadcopter.sdf}
-gz sim "$(pwd)/sim/worlds/$WORLD_FILE" &
+gz sim -r "$(pwd)/sim/worlds/$WORLD_FILE" &
 
 sleep 3
 
